622bb6f76c68ab35c9a1eae026603c6a02f2a805
ibrahim shittu <ibshittu01@gmail.com>
2022-03-29 16:40:31 +0100

    ch(deploy) push to heroku

--- src/api/pictureUpload/pictureUpload/settings.py
+++ src/api/pictureUpload/pictureUpload/settings.py
@@ -116,11 +116,14 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
